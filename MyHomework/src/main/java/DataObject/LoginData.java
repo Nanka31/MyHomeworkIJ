c2a31a0721d682b8a454fe9f1181eac4b51653f5
Nanka31 <141784237+Nanka31@users.noreply.github.com>
2023-08-16 19:15:16 +0400

    Add files via upload

---
+++ MyHomework/src/main/java/DataObject/LoginData.java
@@ -0,0 +1,11 @@
+package DataObject;
+
+public interface LoginData {
+
+    String
+
+            invalidEmailData = "nnnnnnnn@ge",
+            invalidPasswordData = "0001111",
+            validEmailData = "nanka.tughushi@gmail.com";
+
+}
---
+++ MyHomework/src/main/java/PageObject/LogInPage.java
@@ -0,0 +1,18 @@
+package PageObject;
+import org.openqa.selenium.By;
+
+public class LogInPage {
+
+
+    protected By
+
+
+         emailField = By.name("username"),
+         passwordField = By.id("inputPassword"),
+            logInButton = By.id("login");
+
+         //emailField = By.name("email"),
+          //passwordField = By.id("pass"),
+          //logInButton = By.name("login");
+
+}
